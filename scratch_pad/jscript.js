b66e4116acb8603473aa57e81ef82c3d21dba651
Nicholas Mehrle <nmehrle@gmail.com>
2015-01-29 00:52:26 +0100

    finished scratch pad! :^)

--- scratch_pad/jscript.js
+++ scratch_pad/jscript.js
@@ -1,23 +1,93 @@
 $(document).ready(function() {
-	
-
-	var numRows = 30;
-	var numCols = 30;
-	pixWidth = 600/numCols;
-	pixHeight = 600/numRows;
-    for(var i =0; i<numRows; i++) {
-    	$('#gridWrapper').append('<div class="row"></div>');
-    }
-    for(var j=0; j<numCols; j++){
-    	var toAppend = '<div class="pixel"> </div>';
-    	$('.row').append(toAppend);
-   	}
-   	var $pixel = $('.pixel');
-   	$pixel.css("width",pixWidth).css("height",pixHeight);
-   	$pixel.mouseenter(function() {
-   		$(this).css('background-color',"#000");
-   	});
-
-
-
+  makeGrid(20);
+  activateBlack();
 });
+
+function makeGrid(size) {
+  reset();
+  var numPix=size;
+  var pixSize = 600/numPix-2;
+  for(var i =0; i<numPix; i++) {
+    $('#gridWrapper').append('<div class="row"></div>');
+  }
+  for(var j=0; j<numPix; j++){
+    var toAppend = '<div class="pixel"> </div>';
+    $('.row').append(toAppend);
+  }
+  $('.row').height(pixSize);
+  var $pixel = $('.pixel');
+  $pixel.css("width",pixSize).css("height",pixSize);
+};
+
+function resize(col) {
+  var size = prompt("Enter a new grid size");
+  if (size!=null) {
+    makeGrid(size);
+    switch (col) {
+      case 1:
+        activateBlack();
+        break;
+      case 2:
+        activateRandom();
+        break;
+      case 3:
+        activateGreys();
+        break;
+      case 4:
+        activateTrail();
+        break;
+    }
+  }
+};
+
+function reset() {
+  $('.pixel').remove();
+  $('.row').remove();
+};
+
+function activateBlack() {
+  $('.pixel').mouseover(function() {
+    $(this).css('background-color',"#000");
+  });
+};
+
+function activateRandom() {
+  $('.pixel').mouseover(function() {
+    $(this).css('background-color',getRandomColor());
+  });
+};
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+function activateGreys() {
+  var letters = '0123456789abcdef'.split('');
+  $('.pixel').mouseover(function() {
+    var curCol = $(this).css('background-color');
+    var curInd = letters.indexOf(parseRGB(curCol));
+    if(curInd ===0) return;
+    var nextVal = letters[curInd-1];
+    var nextCol = '#'+nextVal+nextVal+nextVal;
+    $(this).css('background-color',nextCol);
+  });
+};
+
+function parseRGB(rgb) {
+  var rgb = rgb.toString().split(',');
+  var r   = rgb[0].substring(4)*1;
+  return r.toString(16).substring(1);
+};
+
+function activateTrail() {
+  $('.pixel').hover(function() {
+    $(this).css('background-color','#000');
+  },function() {
+    $(this).animate({backgroundColor:"#fff"},800);
+  });
+};
